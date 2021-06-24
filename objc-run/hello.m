2a8f70ae185aa527b8eacf57627a008596e7c652
胡争辉 <hu@daonao.com>
2021-06-24 18:57:22 +0800

    add objc-run

---
+++ objc-run/hello.m
@@ -0,0 +1,8 @@
+#!/usr/bin/env objc-run
+
+#import <stdio.h>
+
+int main( int argc, const char *argv[] ) {
+    printf( "hello world\n" );
+    return 0;
+}
---
+++ objc-run/script with weird name.m
@@ -0,0 +1,7 @@
+#!/usr/bin/env objc-run
+#import <stdio.h>
+
+int main( int argc, const char *argv[] ) {
+    printf( "Wow you ran me even if I have spaces in name!\n" );
+    return 0;
+}
